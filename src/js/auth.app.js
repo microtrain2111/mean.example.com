573638ed17b3e4f3d5f35936fa72ae52158cab6c
chad svastisalee <swasgrafx@gmail.com>
2021-12-23 11:01:40 -0600

    Add error/success handling to the login form

--- src/js/auth.app.js
+++ src/js/auth.app.js
@@ -50,8 +50,13 @@ function postRequest(formId, url){
       xhr.send(JSON.stringify(object));
       xhr.onload = function(){
         let data = JSON.parse(xhr.response);
-        console.log(data);
-      }
+        // console.log(data);
+        if(data.success===true){
+            window.location.href = '/';
+          }else{
+            document.getElementById('formMsg').style.display='block';
+          }
+        }
     });
   }
   
